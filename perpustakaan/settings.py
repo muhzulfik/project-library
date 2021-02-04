4404f429dde9501f15a0ec865c795f5af88f701f
muhzulfik <muhzulfik13@gmail.com>
2021-02-04 18:57:27 +0700

    commit

--- perpustakaan/settings.py
+++ perpustakaan/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'perpustakaan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'perpustakaan',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
