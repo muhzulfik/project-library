cfa0c6461436f5c5cd543311591b9e279ed6d5b2
muhzulfik <muhzulfik13@gmail.com>
2021-02-04 19:58:13 +0700

    commit

--- perpustakaan/settings.py
+++ perpustakaan/settings.py
@@ -128,3 +128,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
