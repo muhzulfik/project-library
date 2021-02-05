3513efead4310e04fea13c85aa041e73a93ae2cc
muhzulfik <muhzulfik13@gmail.com>
2021-02-05 14:05:59 +0700

    commit

--- layanan/forms.py
+++ layanan/forms.py
@@ -13,7 +13,7 @@ class catalogForm(forms.ModelForm):
             'katalog_jurusan' : forms.Select({'class':'form-select', 'type':'text','name':'katalog_jurusan', 'id':'katalog_jurusan'}),
             'penulis' : forms.TextInput({'class':'form-control', 'type':'text' ,'name':'penulis', 'placeholder':'Penulis', 'id':'penulis'}),
             'keyword' : forms.TextInput({'class':'form-control', 'type':'search' ,'name':'keyword', 'placeholder':'Keyword', 'id':'keyword'}),
-            'jumlah_buku' : forms.TextInput({'class':'form-control', 'type':'search', 'name':'jumlah_buku', 'placeholder':'Jumlah Buku', 'id':'jumlah_buku'}),
+            'jumlah_buku' : forms.NumberInput({'class':'form-control', 'type':'search', 'name':'jumlah_buku', 'placeholder':'Jumlah Buku', 'id':'jumlah_buku'}),
             'bahasa' : forms.Select({'class':'form-select', 'type':'search','name':'bahasa', 'id':'bahasa'}),
         }
 
@@ -26,10 +26,10 @@ class journalForm(forms.ModelForm):
         widgets = {
             'judul_artikel' : forms.TextInput({'class':'form-control', 'type':'search','name':'judul_artikel', 'placeholder':'Judul Artikel'}),
             'judul_terbitan' : forms.TextInput({'class':'form-control', 'type':'search','name':'judul_terbitan', 'placeholder':'Judul Terbitan'}),
-            'issn' : forms.TextInput({'class':'form-control', 'type':'search','name':'issn', 'placeholder':'ISSN'}),
+            'issn' : forms.NumberInput({'class':'form-control', 'type':'search','name':'issn', 'placeholder':'ISSN'}),
             'bahasa' : forms.Select({'class':'form-select', 'type':'search','name':'bahasa', 'placeholder':'Bahasa'}),
             'tempat_terbit' : forms.TextInput({'class':'form-control', 'type':'search','name':'tempat_terbit', 'placeholder':'Tempat Terbit'}),
-            'tahun' : forms.TextInput({'class':'form-control', 'type':'search','name':'tahun', 'placeholder':'Tahun'}),
+            'tahun' : forms.NumberInput({'class':'form-control', 'type':'search','name':'tahun', 'placeholder':'Tahun'}),
             'volume' : forms.TextInput({'class':'form-control', 'type':'search','name':'volume', 'placeholder':'Volume'}),
             'penerbit' : forms.TextInput({'class':'form-control', 'type':'search','name':'penerbit', 'placeholder':'Penerbit'}),
             'frekuensi_penerbitan' : forms.TextInput({'class':'form-control', 'type':'search','name':'frekuensi_penerbitan', 'placeholder':'Frekuensi Penerbitan'}),
